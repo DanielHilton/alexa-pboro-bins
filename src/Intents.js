402f3a11e59a9bd9047d3ffd62c606bafad9e66a
Daniel Hilton <evolutionviii@hotmail.co.uk>
2017-07-31 22:21:34 +0100

    Added in extra intents ready for development

--- src/Intents.js
+++ src/Intents.js
@@ -2,6 +2,8 @@
 
 /* Get Bin Collection Day Intent */
 const GET_BIN_DAY = "GetBinDay";
+const GET_BIN_COLLECTION_SCHEDULE = "GetBinCollectionSchedule";
+const GET_NEXT_BIN_COLLECTION_DETAILS = "GetNextBinCollectionDetails";
 
 const AMAZON_HELP = "AMAZON.HelpIntent";
 const AMAZON_CANCEL = "AMAZON.CancelIntent";
@@ -9,6 +11,8 @@ const AMAZON_STOP = "AMAZON.StopIntent";
 
 module.exports = {
     "GET_BIN_DAY": GET_BIN_DAY,
+    "GET_BIN_COLLECTION_SCHEDULE": GET_BIN_COLLECTION_SCHEDULE,
+    "GET_NEXT_BIN_COLLECTION_DETAILS": GET_NEXT_BIN_COLLECTION_DETAILS,
     "AMAZON_HELP": AMAZON_HELP,
     "AMAZON_CANCEL": AMAZON_CANCEL,
     "AMAZON_STOP": AMAZON_STOP
